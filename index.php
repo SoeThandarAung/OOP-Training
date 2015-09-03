f2f12698a57b0d639c612514a1bc35ddb68900f5
SoeThandarAung <jassicagugu@gmail.com>
2015-09-03 21:22:36 +0630

    for public private scope lesson

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 	require 'lesson2_user.php';
 	$joost = new User();
-	$joost->email = "joost@tutsplus.com";
-	$joost->password = "ghv7%Lsv7";
-
-	$nick = new User();
-	$nick->email = "nick@gmail.com";
-	$nick->password="nick";
-	var_dump($nick->login());
+	$joost->setPassword('1234lsddfd');
+	var_dump($joost->getPassword());
--- lesson2_user.php
+++ lesson2_user.php
@@ -1,11 +1,33 @@
 <?php
 	class User{
-		public $email;
-		public $password;
+		private $email;
+		private $password;
+		const MINCHARS = 8;
 		public function login(){
 			return 'Logging in';
 		}
 		public function logout(){
 			return 'Logging out';
+		}
+		public function setPassword($string){
+			if($this->validatePassword($string)== false){
+				throw new Exception('The password should be at least' .self::MINCHARS.'character logn');
+			}
+			$this->password = hash('sha256',$string);
+		}
+		public function getPassword(){
+			return $this->password;
+		}
+		public function set(){
+			if(!filter_var($trsing, FILTER_VALIDATE_EMAIL)){
+				throw new Exception('Please Provide a valid email');
+			}
+			$this->email = $string
+		}
+		public function getEmail(){
+			return $this->email;
+		}
+		private function validatePassword($string){
+			return strlen($string) < self::MINCHAR ? false:true;
 		}
 	}
